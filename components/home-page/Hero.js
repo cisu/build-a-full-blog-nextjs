1f02ccfe2b976dbe424b76d9078f76950a71a8c6
george <geor@webdi.com>
2021-08-29 12:52:02 +0300

    create the hero-page

--- components/home-page/Hero.js
+++ components/home-page/Hero.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from './hero.module.css'
 
+import Image from 'next/image';
+
 const Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <section className={styles.hero}>
+            <div className={styles.image}>
+                <Image 
+                src="/images/site/hero-image.jpg"
+                alt="an hero image"
+                width={300}
+                height={300}
+                />
+            </div>
+            <h1>Hi I am George</h1>
+            <p>I blog about web development - especially frontend framework like react and nextJs</p>
+        </section>
     )
 }
 
